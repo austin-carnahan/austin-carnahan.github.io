109574c7f2bbf290ce23155f5a6c2e33469a1882
Austin C <ac6404@gmail.com>
2017-10-09 20:48:10 -0500

    more link updates

--- js/helper.js
+++ js/helper.js
@@ -19,7 +19,7 @@ var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%conta
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><a href="http://www.github.com/racoonmoonswoon" target="_blank"><span class="orange-text">github</span><span class="white-text">%data%</span></a></li>';
+var HTMLgithub = '<li class="flex-item"><a href="http://www.github.com/austin-carnahan" target="_blank"><span class="orange-text">github</span><span class="white-text">%data%</span></a></li>';
 var HTMLlinkedIn = '<li class="flex-item"><a href="http://www.linkedin.com/in/austincarnahan" target="_blank"><span class="orange-text">LinkedIn</span><span class="white-text">%data%</span></a></li>';
 // var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
